90acc9d93b1f595cd22eb75f2a03eb2ba38a78eb
Lena G <helenagroza619@gmail.com>
2022-05-25 21:54:19 +0100

    Adding questions works (but not options)

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -15,6 +15,7 @@ enum QuestionType {
 
 export class CreateComponent implements OnInit {
 
+  fb: FormBuilder; 
   faDelete = faTrash;
   faCopy = faCopy; 
   faUploadImage = faImage; 
@@ -23,48 +24,83 @@ export class CreateComponent implements OnInit {
   eQuestionType = QuestionType; // enum 
 
   defaultQuestionType = QuestionType.Radio; 
-  //testForm: FormGroup; 
-  questionForm: FormGroup;  
+  testForm: FormGroup; 
+  //questionForm: FormGroup;  
 
     // getters to easily access form controls 
-  get questionName(){
-    return this.questionForm.get("questionName"); 
+  get testName(){
+    return this.testForm.get("testName"); 
   }
-  get options(){
-    return this.questionForm.get("options") as FormArray; 
+  get testSubject(){
+    return this.testForm.get("testSubject"); 
   }
+  get questions(){
+    return this.testForm.get("questions") as FormArray; 
+  }
+  // get questionName(){
+    
+  //   return this.testForm.get("questionName"); 
+  //   //return this.questionForm.get("questionName"); 
+  // }
+  // get options(){
+  //   return this.questionForm.get("options") as FormArray; 
+  // }
   
 
   constructor(fb: FormBuilder) {
-    this.questionForm = new FormGroup({
-      questionName: new FormControl(''), 
-      options: new FormArray([
-        //this.addOption() 
-      ])
+    // this.questionForm = new FormGroup({
+    //   questionName: new FormControl(''), 
+    //   options: new FormArray([
+    //     //this.addOption() 
+    //   ])
+    // }); 
+    this.fb = fb; // so it can be used anywhere in the class, not just in ctor
+    this.testForm = fb.group({
+      testName: [''], 
+      testSubject: [''],
+      questions: fb.array([
+        //individual question (with options)
+        fb.group({ //if it's within an array, it doesn't have a name, only an index! 
+          questionName: [''], 
+          questionType: [this.eQuestionType.Radio], //default value for question type 
+          options: fb.array([]),
+          // other fields? required etc  
+        })
+      ]), 
     }); 
-
-  //   this.testForm = new FormGroup({
-  //     testName: new FormControl(''), 
-
-  //  }); 
+  
 
    }
 
   ngOnInit(): void {
   }
 
-  // method to dynamically insert options into the FormArray: 
-  addOption(): FormControl{
-    let fc = new FormControl(); 
-    this.options.push(fc); 
-    return fc; 
+  addQuestion(){
+    this.questions.push(
+      this.fb.group({ 
+        questionName: [''], 
+        questionType: [this.eQuestionType.Radio],  
+        options: this.fb.array([]),
+        // other fields? required etc  
+      })
+    ); //maybe has to have content inside? 
   }
 
-  removeOption(idx: number): void{ //option: FormControl
-    //let index = this.options.controls.indexOf(option); 
-    //this.options.removeAt(index); 
-    this.options.removeAt(idx); 
+  removeQuestion(idx: number){
+    this.questions.removeAt(idx);
   }
+  // method to dynamically insert options into the FormArray: 
+  // addOption(): FormControl{ // have to add option *to a specific question*
+  //   let fc = new FormControl(); 
+  //   this.options.push(fc); 
+  //   return fc; 
+  // }
+
+  // removeOption(idx: number): void{ //option: FormControl
+  //   //let index = this.options.controls.indexOf(option); 
+  //   //this.options.removeAt(index); 
+  //   this.options.removeAt(idx); 
+  // }
 
   onQuestionTypeChange(){
 
@@ -80,3 +116,22 @@ export class CreateComponent implements OnInit {
   //     {id: 2, name: 'Multiple Choice: Multiple Answers'}, 
   //     {id: 3, name: 'Short Text'}, 
   //   ]; 
+
+
+
+    // this.testForm = new FormGroup({
+    //   testName: new FormControl(''), 
+    //   testSubject: new FormControl(''), 
+    //   questions: new FormArray([
+
+    //     questionForm: new FormGroup({
+    //       questionName: new FormControl(''), 
+    //       options: new FormArray([
+    //           //this.addOption() 
+    //         ])
+
+    //     })
+    //   ])
+
+
+  //  }); 
